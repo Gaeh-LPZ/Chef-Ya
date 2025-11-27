cbdfc09e170f9bcc8f4445e8c80bcba3ce906201
Gaeh-LPZ <galob567@gmail.com>
2025-11-27 09:48:49 -0600

    back to the deployed server

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const API_BASE_URL = 'http://localhost:8000';
+    const API_BASE_URL = 'https://chef-ya-api.onrender.com';
 
     // ==========================
     //   UBICACIÓN EN HEADER
