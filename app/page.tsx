ba2c3fe3178b7a7cabd8fc8b2a7f5d546356862c
Gaeh-LPZ <galob567@gmail.com>
2026-04-01 15:16:51 -0600

    Home Page made

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,47 @@
+import { konkhmer, johmuria, lilita } from "./ui/fonts";
 import Image from "next/image";
 
-export default function Home() {
+export default function Page() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+    <>
+      <Image src="/bg-1.png" alt="background image" className="absolute top-0 left-0 w-8/12 h-full object-cover z-[-1]" width={1024} height={1536} loading="eager"/>
+      <Image src="/bg-2.png" alt="hamburger background image" className="absolute top-0 right-0 w-4/12 h-full object-cover z-[-1]" width={500} height={666}/>
+      <header className="flex flex-row justify-between w-3/5 m-6 row-span-1">
+        <img src="/burger.svg" alt="burger icon for more options" width={32} height={32} />
+        <nav className={`${konkhmer.className} flex flex-row gap-4`}>
+          <a href="#" className="bg-beige p-1.5 rounded-md">Iniciar Sesión</a>
+          <a href="#" className="text-white bg-black p-1.5 rounded-md">Registrarse</a>
+        </nav>
+      </header>
+      <main className="flex flex-row justify-around items-center row-span-2">
+        <aside>
+          <h2 className={`${johmuria.className} text-6xl text-white`}>¡Ordena ahora mismo!</h2>
+          <form action="" className="flex flex-row gap-2">
+            <label className="flex flex-row bg-beige w-full gap-2 p-1.5 rounded-md">
+              <img src="/location.svg" alt="Location icon for more accesibility" width={24} height={24} />
+              <input type="text" name="ubicacion" placeholder="Ingresa la dirección de entrega" className="w-full" />
+            </label>
+            <button type="submit" className="flex flex-row items-center bg-black w-34 p-1.5 text-white gap-2 rounded-md">
+              Buscar
+              <svg width={24} height={24} aria-label="search icon">
+                <use xlinkHref="/search.svg" className="stroke-white fill-white"></use>
+              </svg>
+            </button>
+          </form>
+        </aside>
+        <div className="flex flex-col gap-2">
+          <h2 className={`${lilita.className} text-5xl text-white`}>Categorías</h2>
+          <form action="">
+            <label className="bg-beige flex flex-row rounded-md p-1.5">
+              <input type="text" name="categoria" placeholder="Buscar comida" />
+              <Image src="/search.svg" alt="Search icon" width={24} height={24}/>
+            </label>
+          </form>
         </div>
       </main>
-    </div>
+      <footer  className="row-span-1 p-1">
+        <Image src="/Chef-Ya-Logo.png" alt="Chef-Ya Logo" width={264} height={110}/>
+      </footer>
+    </>
   );
 }
