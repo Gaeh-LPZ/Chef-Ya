c5d8f23c16b8e2a399b6c306c8b032cfec4d48a5
Gaeh-LPZ <galob567@gmail.com>
2026-04-08 15:39:45 -0600

    sign in page view finished

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from "next";
-import Header from "../ui/Header";
 import { konkhmer } from "../ui/fonts";
 
 export const metadata: Metadata = {
@@ -13,17 +12,27 @@ export default function Page() {
         <img src="/bg-sign-in.jpg" alt="Chef Ya! Curated Dish" className="absolute inset-0 w-full h-full object-cover" />
         <p className={`${konkhmer.className} text-white text-xl tracking-tighter bg-white/10 backdrop-blur-md px-6 py-4 rounded-full w-fit border border-white/20 top-12 left-12 absolute`}><strong>Chef Ya!</strong></p>
       </aside>
-      <section>
-        <header>
-          <h1>Bienvenido de vuelta</h1>
-          <p>Accede a tus menus preferidos y rastrea tus pedidos</p>
+      <section className="bg-surface flex flex-col items-center justify-center px-6 py-12 md:px-28 lg:px-60 relative h-screen gap-4">
+        <header className="flex flex-col gap-1.5 w-full">
+          <h1 className={`${konkhmer.className} text-4xl font-bold`}>Bienvenido de vuelta</h1>
+          <p className="text-gray-500 text-base">Accede a tus menus preferidos y rastrea tus pedidos</p>
         </header>
-        <form action="">
-          <label>
-            <span className=" material-symbols-outlined text-outline" data-icon="mail">mail</span>
-            Correo Electronico
-            <input type="text" name="email" placeholder="name@example.com"/>
+        <form action="" className="w-full flex flex-col gap-6">
+          <label className="text-gray-600 text-sm flex flex-col gap-1">
+            CORREO ELECTRONICO
+            <div className="flex flex-row gap-4 bg-white rounded-full p-2">
+              <span className=" material-symbols-outlined text-outline" data-icon="mail">mail</span>
+              <input type="text" name="email" placeholder="name@example.com" />
+            </div>
           </label>
+          <label className="text-gray-600 text-sm flex flex-col gap-1">
+            CONTRASEÑA
+            <div className="flex flex-row gap-4 bg-white rounded-full p-2">
+              <span className="material-symbols-outlined text-outline" data-icon="lock">lock</span>
+              <input type="password" name="password" placeholder="********"/>
+            </div>
+          </label>
+          <button className="w-full py-5 bg-linear-to-br from-primary to-primary-container text-white/90 font-bold text-lg rounded-full editorial-shadow hover:scale-[1.02] active:scale-95 transition-all duration-300">Inciar Sesion</button>
         </form>
       </section>
     </main>
