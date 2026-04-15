e40cf14c51f631a36a0680dbfc191fe40d113df1
Gaeh-LPZ <galob567@gmail.com>
2026-04-15 16:57:27 -0600

    fixed color in the header

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -3,16 +3,16 @@ export default function Header(){
         <header className="flex flex-row h-full justify-around p-3 items-center">
             <img src="/Chef-Ya-Logo.png" alt="Chef Ya Logo" width={132} height={55}/>
             <div className="flex flex-row gap-1.5 w-fit p-2 items-center">
-                <svg width={32} height={32} aria-label="location icon" className="bg-primary rounded-full p-1.5 text-white">
+                <svg width={32} height={32} aria-label="location icon" className="bg-secondary rounded-full p-1.5 text-white">
                     <use xlinkHref="/location.svg"></use>
                 </svg>
                 <aside>
                     <p className="text-sm text-gray-400"><small>Lugar de envio</small></p>
-                    <p className="text-primary text-sm">Lugar CP</p>
+                    <p className="text-secondary text-sm">Lugar CP</p>
                 </aside>
             </div>
             <form action="" className="hidden md:flex md:w-xl">
-                <label className="flex flex-row gap-1 bg-primary w-full p-2 rounded-full">
+                <label className="flex flex-row gap-1 bg-secondary w-full p-2 rounded-full">
                     <svg width={24} height={24} aria-label="Search icon" className="text-white">
                         <use xlinkHref="/search.svg"></use>
                     </svg>
